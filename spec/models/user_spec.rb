19b41db1713b28013136be573ce65481afffc257
VickyD <vicky17d@gmail.com>
2013-03-03 21:52:53 -0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
+#
+
 require 'spec_helper'
 
 describe User do
@@ -11,6 +23,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   
   it { should be_valid }
@@ -99,5 +112,8 @@ describe User do
     end
   end
 
-
+  describe "remember_token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
