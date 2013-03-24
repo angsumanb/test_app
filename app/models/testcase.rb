297f20772c612c77540126d4f5b3476bac12c085
VickyD <vicky17d@gmail.com>
2013-03-24 15:21:30 -0700

    Change some layout for testcases pages

--- app/models/testcase.rb
+++ app/models/testcase.rb
@@ -2,12 +2,16 @@ class Testcase < ActiveRecord::Base
   attr_accessible :comments, :estimate, :expectedresult, :preconditions, :priority, :steps, :title, :testtype
 
   belongs_to :suite
+  TTOPTIONS = ['Functionality', 'Regression', 'Performance', 'Usability', 'Other']
+  PROPTIONS = ['High', 'Medium', 'Low', 'Unknown']
 
   validates :title, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
 
   validates :steps, presence: true
   validates :testtype, presence: true
   validates :priority, presence: true
+  validates_inclusion_of :testtype, :in => TTOPTIONS
+  validates_inclusion_of :priority, :in => PROPTIONS
 
   validates :suite_id, presence: true
 
