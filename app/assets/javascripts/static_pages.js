bc82406a311188e106c030c601c3b2034f60b904
VickyD <vicky17d@gmail.com>
2013-05-05 21:59:18 -0700

    Do sidebar

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,11 @@
+/*$(document).ready( function() {
+	$('.nav-pills').on('click', 'li', function(e) {
+	  $('.nav-pills li').removeClass('active');
+      var $this = $(this);
+      if (!$this.hasClass('active')) {
+      $this.addClass('active');
+    }
+  //e.preventDefault();
+  });
+});
+*/
