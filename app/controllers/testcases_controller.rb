4b7c5ffda9fec256dfc660ff4af4b21344da38c3
VickyD <vicky17d@gmail.com>
2013-06-07 19:55:32 -0700

    File upload & ui

--- app/controllers/testcases_controller.rb
+++ app/controllers/testcases_controller.rb
@@ -6,6 +6,13 @@ class TestcasesController < ApplicationController
   def show
     @testcase = Testcase.find(params[:id])
     @suite = Suite.find(@testcase.suite)
+    @attachmentCount = 0
+    if(@testcase.photo?)
+      @attachmentCount = @attachmentCount+1
+    end
+    if(@testcase.zip?)
+      @attachmentCount = @attachmentCount+1
+    end
   end
   
   def new
@@ -24,7 +31,7 @@ class TestcasesController < ApplicationController
   def create
     @suite = Suite.find_by_id(params[:testcase][:suite_id])
 #    @testcase = @suite.testcases.build(params[:testcase])
-    @testcase = @suite.testcases.build(title: params[:testcase][:title], steps: params[:testcase][:steps], testtype: params[:testcase][:testtype], priority: params[:testcase][:priority], estimate: params[:testcase][:estimate])
+    @testcase = @suite.testcases.build(title: params[:testcase][:title], steps: params[:testcase][:steps], testtype: params[:testcase][:testtype], priority: params[:testcase][:priority], estimate: params[:testcase][:estimate], photo: params[:testcase][:photo], zip: params[:testcase][:zip])
 #    @testcase = @suite.testcases.build(title: params[:testcase])
     if @testcase.save
       flash[:success] = "Congratulaions! You have created a new testcase."
--- app/models/testcase.rb
+++ app/models/testcase.rb
@@ -1,12 +1,17 @@
 class Testcase < ActiveRecord::Base
-  attr_accessible :comments, :estimate, :expectedresult, :preconditions, :priority, :steps, :title, :testtype
+  attr_accessible :zip, :photo, :comments, :estimate, :expectedresult, :preconditions, :priority, :steps, :title, :testtype
+
+  has_attached_file :photo , :styles =>       { :small => "50x50>" }, :url  => "/assets/testcases/:id/:style/:basename.:extension", :path => ":rails_root/public/assets/testcases/:id/:style/:basename.:extension"
+
+  has_attached_file :zip ,  :url  => "/assets/testcases/:id/:style/:basename.:extension", :path => ":rails_root/public/assets/testcases/:id/:style/:basename.:extension"
+  
 
   belongs_to :suite
   has_many :testresults, dependent: :destroy
   has_many :testruns, :through => :testresults
 
-   before_save { |testcase| testcase.title = title.capitalize }  
-#  accepts_nested_attributes_for :testresults
+
+  before_save { |testcase| testcase.title = title.capitalize }  
 
 
   TTOPTIONS = ['Functionality', 'Regression', 'Performance', 'Usability', 'Other']
@@ -14,7 +19,7 @@ class Testcase < ActiveRecord::Base
 
   validates :title, presence: true, length: { maximum: 200 }, uniqueness: { case_sensitive: false }
 
-  validates :steps, presence: true
+#  validates :steps, presence: true
   validates :testtype, presence: true
   validates :priority, presence: true
   validates_inclusion_of :testtype, :in => TTOPTIONS
